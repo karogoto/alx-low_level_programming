865d0f4856ae97c583fd0dd3971b69d2f09ff696
karogoto <karogotomugweru@gmail.com>
2023-02-19 03:31:46 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry
- * Return: 1
+ * main - prints a quote to the standard error
+ *
+ * Return: Always 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" -Dora Korpar, 2025-10-19\n", 59);
-	return (1);
+char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = 0;
+while (quote[len] != '\0')
+{
+len++;
+}
+write(STDERR_FILENO, quote, len);
+return (1);
 }

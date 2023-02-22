11cc8972270651a385dc2e710b51f60b2106959e
karogoto <karogotomugweru@gmail.com>
2023-02-22 00:46:09 -0800

    gg

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 int _putchar(char c);
-
+int _abs(int n);
+int print_last_digit(int)
 void print_putchar(void);
+int print_sign(int n);
 void jack_bauer(void);
-
+void print_alphabet_x10(void);
 #endif /* MAIN_H */
 

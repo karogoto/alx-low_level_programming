a13d75f6c93068743f2a81f2b5c00028431d0ffc
karogoto <karogotomugweru@gmail.com>
2023-02-24 01:15:22 -0800

    hard

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,13 +2,13 @@
 
 /**
  * _isdigit - checks if a character is a digit
- * @c: character to check
+ * @c: the character to check
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-if (c >= '0' && c <= '9'
+if (c >= '0' && c <= '9')
 return (1);
 else
 return (0);

4f8056a67bc41918312f244b6c0fb4d39d678974
karogoto <karogotomugweru@gmail.com>
2023-02-27 01:09:04 -0800

    hard things

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,8 @@ int _putchar(char c);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void print_rev(char *s);
+void print_rev(char *s)i;
+void rev_string(char *s);
 
 #endif /* MAIN_H */
 

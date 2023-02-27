9142c9760a6a20e8901288c4dd8edb07a84c7e7b
karogoto <karogotomugweru@gmail.com>
2023-02-27 01:55:06 -0800

    hard stuff

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,18 +8,18 @@
  */
 void puts_half(char *str)
 {
-	int len = strlen(str);
-	int i;
+        int len = strlen(str);
+        int i;
 
-	if (len % 2 == 0)
-	{
-		for (i = len / 2; i < len; i++)
-			_putchar(str[i]);
-	}
-	else
-	{
-		for (i = (len - 1) / 2; i < len; i++)
-			_putchar(str[i]);
-	}
-	_putchar('\n');
+        if (len % 2 == 0)
+        {
+                for (i = len / 2; i < len; i++)
+                        putchar(str[i]);
+        }
+        else
+        {
+                for (i = (len + 1) / 2; i < len; i++)
+                        putchar(str[i]);
+        }
+        putchar('\n');
 }

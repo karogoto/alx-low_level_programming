7d652064bf6b44b53b1a84b482c78497e254dd58
karogoto <karogotomugweru@gmail.com>
2023-02-28 00:39:31 -0800

    hard stuff

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,30 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * generate_password - Generates a random password for 101-crackme
+ * generate_password - Generates a random valid password for 101-crackme
  *
  * Return: The generated password
  */
 char *generate_password(void)
 {
-  static char password[100];
-  int i, sum;
-
+  static char password[16];
+  int i, sum = 0;
+  
   /* Seed the random number generator */
   srand(time(0));
 
-  /* Generate random characters until the sum of their ASCII values is 2772 */
-  sum = 0;
-  i = 0;
-  while (sum < 2772)
-    {
-      password[i] = rand() % 94 + 33; /* Printable ASCII characters range from 33 to 126 */
-      sum += password[i];
-      i++;
-    }
-  password[i] = '\0';
+  /* Generate 14 random characters */
+  for (i = 0; i < 14; i++) {
+    password[i] = rand() % 94 + 33; /* printable ASCII characters between 33 and 126 */
+    sum += password[i];
+  }
+
+  /* Calculate the ASCII value of the 15th character */
+  password[14] = 2772 - sum;
+
+  /* Add null terminator */
+  password[15] = '\0';
 
   return password;
 }
